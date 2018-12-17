9ae9bdf21dce731950f4fb0d2f4da8f2b275b3e6
marwafriaa <friaamarwa14@gmail.com>
2018-12-17 12:19:31 +0100

    add Util for spark configuration

--- src/main/scala/utils/Util.scala
+++ src/main/scala/utils/Util.scala
@@ -1,5 +1,14 @@
 package utils
 
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.{SparkConf, SparkContext}
+
 class Util {
+
+  val conf = new SparkConf().setMaster("local").setAppName("ratingCalculation")
+  // Create a Scala Spark Context.
+  val sc = new SparkContext(conf)
+  val sqlContext = new SQLContext(sc)
+  val spark=sqlContext.sparkSession
 
 }

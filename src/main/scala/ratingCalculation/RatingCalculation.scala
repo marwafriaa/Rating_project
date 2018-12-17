688385bb79e32028199c1db968b59c83d59eb149
marwafriaa <friaamarwa14@gmail.com>
2018-12-17 12:21:32 +0100

    add RatingCalculation methods (main)

--- src/main/scala/ratingCalculation/RatingCalculation.scala
+++ src/main/scala/ratingCalculation/RatingCalculation.scala
@@ -1,5 +1,23 @@
 package ratingCalculation
 
+import utils.Util
+import idGeneration.IdGeneration
 object RatingCalculation {
+
+  def main(args: Array[String]): Unit = {
+    var util = new Util()
+    var idg = new IdGeneration
+
+    val cleaned_id_df = idg.generate_new_id_integers(util,args(0))
+    cleaned_id_df.show()
+    //val res2 = calculate_rating(util,cleaned_id_df)
+    //res2.show()
+
+
+
+
+
+  }
+
 
 }
